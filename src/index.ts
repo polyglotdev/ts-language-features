b165aac413f32b18a4edc9f856a7426ea3b9999e
Dom Hallan <dom@polyglotdev.com>
2020-12-30 18:25:43 -0600

    🎆 initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+export type StudentId = string;
+
+const firstStudent: StudentId = `5`;
+const lastStudent: StudentId = `123`;
+
+let x: number;
+let y: Number;
+
+// x = y  this would produce an error.
+
+// inferred number
+const pi = Math.PI;
+
+// inferred number
+const year = 2020;
+
+// explicit number
+const quantity: number = 92;
